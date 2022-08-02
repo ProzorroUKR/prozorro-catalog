036db4e59a54ee91c8e56c93021c578484535450
Serhij Andreev <serhij.andreev@raccoongang.com>
2022-08-02 12:13:28 +0300

    Add document serializer to product serializer

--- src/catalog/serializers/product.py
+++ src/catalog/serializers/product.py
@@ -1,7 +1,12 @@
-from catalog.serializers.base import RootSerializer
+from catalog.serializers.base import RootSerializer, ListSerializer
+from catalog.serializers.document import DocumentSerializer
 
 
 class ProductSerializer(RootSerializer):
+    serializers = {
+        "documents": ListSerializer(DocumentSerializer),
+    }
+
     def __init__(self, data, vendor=None, show_owner=True):
         super().__init__(data, show_owner)
         if vendor and 'vendor' in data:
