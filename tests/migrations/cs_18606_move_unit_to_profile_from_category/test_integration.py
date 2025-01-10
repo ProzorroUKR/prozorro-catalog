2455274619363bb27cc49d12801e5b9c3d72e4e3
Olha Zamirets <ozamirets@gmail.com>
2025-01-10 12:25:07 +0200

    [CS-18606] Move requirement unit to profile from category

---
+++ tests/migrations/cs_18606_move_unit_to_profile_from_category/test_integration.py
@@ -0,0 +1,159 @@
+from copy import deepcopy
+from uuid import uuid4
+
+from catalog.migrations.cs_18606_move_unit_to_profile_from_category import (
+    migrate_profiles,
+)
+from tests.integration.conftest import api, db, get_fixture_json
+
+
+async def test_requirements_number(db, api):
+    category = deepcopy(get_fixture_json('category'))
+    category["criteria"] = [
+        {
+            "title": "Технічні характеристики предмета закупівлі",
+            "description": "Яйця столові курячі",
+            "id": "1f92023591bd4096aea88064eaa4b235",
+            "requirementGroups": [
+                {
+                    "description": "Технічні характеристики",
+                    "id": "f3d2b5995da042ff858a6ea7b5a1a8dd",
+                    "requirements": [{
+                        "title": "Xарактеристика №1",
+                        "dataType": "number",
+                        "id": "8726f95aeb1d4b289d6c1a5a07271c93",
+                        "expectedValue": 10.0,
+                        "unit": {
+                            "code": "CMT",
+                            "name": "см"
+                        }
+                    }, {
+                        "title": "Xарактеристика №2",
+                        "dataType": "number",
+                        "id": "8726f95aeb1d4b289d6c1a5a07271c93",
+                        "minValue": 1.0
+                    }, {
+                        "title": "Xарактеристика №3",
+                        "dataType": "integer",
+                        "id": "8726f95aeb1d4b289d6c1a5a07271c93",
+                        "expectedValue": 20,
+                        "unit": {
+                            "code": "CMT",
+                            "name": "см"
+                        }
+                    }, {
+                        "title": "Xарактеристика №3",
+                        "dataType": "integer",
+                        "id": "8726f95aeb1d4b289d6c1a5a07271c93",
+                        "expectedValue": 10,
+                    }, {
+                        "title": "Xарактеристика №5",
+                        "dataType": "integer",
+                        "id": "8726f95aeb1d4b289d6c1a5a07271c93",
+                        "minValue": 0,
+                        "unit": {
+                            "code": "CMT",
+                            "name": "см"
+                        }
+                    }]
+                }
+            ]
+        }
+    ]
+    await db.category.insert_one(category)
+    profile = deepcopy(get_fixture_json("profile"))
+    profile["_id"] = uuid4().hex
+    profile["relatedCategory"] = category["_id"]
+    profile["dateModified"] = "2024-10-01T11:54:57.860085+03:00"
+    profile["criteria"] = [
+        {
+            "title": "Технічні характеристики предмета закупівлі",
+            "description": "Яйця столові курячі",
+            "id": "1f92023591bd4096aea88064eaa4b235",
+            "requirementGroups": [
+                {
+                    "description": "Технічні характеристики",
+                    "id": "f3d2b5995da042ff858a6ea7b5a1a8dd",
+                    "requirements": [{
+                        "title": "Xарактеристика №1",
+                        "dataType": "number",
+                        "id": "8726f95aeb1d4b289d6c1a5a07271c93",
+                        "expectedValue": 10.0
+                    }, {
+                        "title": "Xарактеристика №2",
+                        "dataType": "number",
+                        "id": "8726f95aeb1d4b289d6c1a5a07271c93",
+                        "minValue": 20.0,
+                    }, {
+                        "title": "Xарактеристика №3",
+                        "dataType": "integer",
+                        "id": "8726f95aeb1d4b289d6c1a5a07271c93",
+                        "expectedValue": 20,
+                        "unit": {
+                            "code": "INH",
+                            "name": "дюйм"
+                        }
+                    }, {
+                        "title": "Xарактеристика №4",
+                        "dataType": "integer",
+                        "id": "8726f95aeb1d4b289d6c1a5a07271c93",
+                        "expectedValue": 10,
+                        "unit": {
+                            "code": "CMT",
+                            "name": "см"
+                        },
+                    }, {
+                        "title": "Xарактеристика №5",
+                        "dataType": "string",
+                        "id": "8726f95aeb1d4b289d6c1a5a07271c93",
+                        "expectedValues": ["0"]
+                    }]
+                }
+            ]
+        }
+    ]
+    await db.profiles.insert_one(profile)
+
+    await migrate_profiles()
+
+    profile_data = await db.profiles.find_one({"_id": profile["_id"]})
+    assert profile_data["criteria"][0]["requirementGroups"][0]["requirements"] == [
+        {
+            "title": "Xарактеристика №1",
+            "dataType": "number",
+            "id": "8726f95aeb1d4b289d6c1a5a07271c93",
+            "expectedValue": 10.0,
+            "unit": {
+                "code": "CMT",
+                "name": "см"
+            },
+        }, {
+            "title": "Xарактеристика №2",
+            "dataType": "number",
+            "id": "8726f95aeb1d4b289d6c1a5a07271c93",
+            "minValue": 20.0,
+        }, {
+            "title": "Xарактеристика №3",
+            "dataType": "integer",
+            "id": "8726f95aeb1d4b289d6c1a5a07271c93",
+            "expectedValue": 20,
+            "unit": {
+                "code": "INH",
+                "name": "дюйм"
+            },
+        }, {
+            "title": "Xарактеристика №4",
+            "dataType": "integer",
+            "id": "8726f95aeb1d4b289d6c1a5a07271c93",
+            "expectedValue": 10,
+            "unit": {
+                "code": "CMT",
+                "name": "см"
+            },
+        }, {
+            "title": "Xарактеристика №5",
+            "dataType": "string",
+            "id": "8726f95aeb1d4b289d6c1a5a07271c93",
+            "expectedValues": ["0"]
+        }
+    ]
