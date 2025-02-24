43a6100c2dfac3d329a55e8d39f4ceb998136d5c
Olha Zamirets <ozamirets@gmail.com>
2025-02-24 15:25:28 +0200

    Freeze setuptools

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ with open('requirements.txt', 'r') as f:
             requires.append("@".join([egg, res]))
 
 setup(
-    name='prozorro_crawler',
+    name='prozorro_catalog',
     use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    setup_requires=['setuptools_scm==8.1.0'],
     description='',
     packages=find_packages('src'),
     package_dir={'': 'src'},
